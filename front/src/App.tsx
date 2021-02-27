307836d046a51c10222cfe8b786d274f5b3f8add
Emmanuel Gomes <aeag@outlook.com>
2021-02-26 21:12:35 -0300

    Controller and Initial front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header, Icon } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Atividades</Header.Content>
+      </Header>
     </div>
   );
 }
