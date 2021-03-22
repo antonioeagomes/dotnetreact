55d28845eefd04ad1856411a106b9780b6ad824c
Emmanuel Gomes <aeag@outlook.com>
2021-03-22 20:25:56 -0300

    AspNetCore.Identity and UserManager

--- back/Persistence/Seed.cs
+++ back/Persistence/Seed.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName = "Bob", UserName = "bob", Email="bob@test.com" }
+                };
+
+                foreach (var item in users)
+                {
+                    await userManager.CreateAsync(item, "Pa$$w0rd");
+                }
+            }
+
+
             if (context.Activities.Any()) return;
-            
+
             var activities = new List<Activity>
             {
                 new Activity
