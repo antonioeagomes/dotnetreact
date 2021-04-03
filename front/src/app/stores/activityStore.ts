946c2e614974c2395e351e91480d38389223663c
Emmanuel Gomes <aeag@outlook.com>
2021-04-03 18:21:49 -0300

    Filtering

--- front/src/app/stores/activityStore.ts
+++ front/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable, reaction, runInAction } from "mobx";
 import { Activity, ActivityFormValues } from "../models/activity";
 import agent from "../api/agent";
 import { store } from "./store";
@@ -13,10 +13,19 @@ export default class ActivityStore {
   loadingInitial = false;
   pagination: Pagination | null = null;
   pagingParams = new PagingParams();
+  predicate = new Map().set('all', true);
 
 
   constructor() {
     makeAutoObservable(this);
+
+    reaction(
+      () => this.predicate.keys(),
+      () => {
+        this.pagingParams = new PagingParams();
+        this.activityRegistry.clear();
+        this.loadActivities();
+      })
   }
 
   setPagingParams = (pagingParams: PagingParams) => {
@@ -27,6 +36,13 @@ export default class ActivityStore {
     const params = new URLSearchParams();
     params.append('pageNumber', this.pagingParams.pageNumber.toString());
     params.append('pageSize', this.pagingParams.pageSize.toString());
+    this.predicate.forEach((value, key) => {
+      if (key === 'startDate') {
+        params.append(key, (value as Date).toISOString());
+      } else {
+        params.append(key, value);
+      }
+    })
     return params;
   }
 
@@ -64,6 +80,38 @@ export default class ActivityStore {
 
   setPagination = (pagination: Pagination) => {
     this.pagination = pagination;
+  }
+
+  setPredicate = (predicate: string, value: string | Date) => {
+    const resetPredicate = () => {
+      this.predicate.forEach((value,key) => {
+        if(key !== 'startDate') this.predicate.delete(key);
+      })
+    }
+    switch (predicate) {
+      case 'all':
+        resetPredicate();
+        this.predicate.set('all', true);
+        break;
+
+      case 'isGoing':
+        resetPredicate();
+        this.predicate.set('isGoing', true);
+        break;
+      
+      case 'isHost':
+        resetPredicate();
+        this.predicate.set('isHost', true);
+        break;
+      
+      case 'startDate':
+        this.predicate.delete('startDate');
+        this.predicate.set('startDate', value);
+        break;
+
+      default:
+        break;
+    }
   }
 
   loadActivity = async (id: string) => {
@@ -172,6 +220,7 @@ export default class ActivityStore {
       this.setLoading(false);
     }
   };
+
   updateAttendance = async () => {
     const user = store.userStore.user;
     this.loading = true;
